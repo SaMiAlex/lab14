ec9f6a6a597cc220fa8bb11cddf0e39d91b59487
SaMiAlex <samias@mail.ru>
2021-10-22 05:17:20 +0600

    Добавьте файлы проекта.

---
+++ lab14/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            cat.ShowInfo();
+            cat.Say();
+            dog.ShowInfo();
+            dog.Say();
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        
+        public abstract void Say();
+        
+        public void ShowInfo()
+        {
+            Console.Write("Меня зовут {0}, ", Name);
+        }
+    }
+
+    class Cat : Animal
+    {
+        string name = "Рыжик";        
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("мяу");
+        }
+
+    }
+    class Dog : Animal
+    {
+        string name = "Бобик";
+
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("гав");
+        }
+
+    }
+}
